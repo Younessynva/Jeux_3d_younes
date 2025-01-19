df328412f27135c8c3853cd732be22ded40ae284
Quentin Deunette <quentin.deunette95@hotmail.fr>
2025-01-19 11:24:01 +0100

    Correction GameOverScreen

--- Assets/GameOverScreen.cs
+++ Assets/GameOverScreen.cs
@@ -3,9 +3,19 @@ using UnityEngine.UI;
 
 public class GameOverScreen : MonoBehaviour
 {
-    public Text pointsText;
-   public void Setup(int score){
-    gameObject.setActive(true);
-    pointsText.text = score.ToString() + "Points";
-   }
+    public Text pointsText; 
+
+    
+    public void Setup(int score)
+    {
+        gameObject.SetActive(true); 
+        if (pointsText != null) 
+        {
+            pointsText.text = score.ToString() + " Points"; 
+        }
+        else
+        {
+            Debug.LogError("pointsText n'est pas assigné dans l'inspecteur Unity."); 
+        }
+    }
 }
