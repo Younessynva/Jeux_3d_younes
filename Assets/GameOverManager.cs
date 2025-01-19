7c6eb893b0178f20559b421b4f19b695a1939ef3
antsa-00 <antsalalaina00@gmail.com>
2025-01-19 05:43:13 +0100

    GameOver-Restart
    Restart Button

---
+++ Assets/GameOverManager.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverManager : MonoBehaviour
+{
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("HomeScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
